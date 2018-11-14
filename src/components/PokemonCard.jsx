0e01ec0770c43649f41606602f1595b832641d46
Dana Ciocan <d.ciocan@dectech.co.uk>
2018-11-14 11:40:59 +0000

    Refactor CSS for PokemonCard component

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import sprites from '../assets/sprites.png';
 import { formatName } from '../utilities/formatting';
+import './PokemonCard.css';
 
 const PokemonCard = ({ monster, index }) => {
   const pokeXPos = -96 * index;
@@ -13,7 +14,7 @@ const PokemonCard = ({ monster, index }) => {
   let pokeId = monster.url.split('/');
   pokeId = pokeId[pokeId.length - 2];
   return (
-    <div className="pokemon" key={pokeId}>
+    <div className="pokemon-card" key={pokeId}>
       <Link to={`/${pokeId}`} style={{ textDecoration: 'none' }}>
         <div className="sprite" style={pokeSprite} />
         <div className="name">{formatName(monster.name)}</div>
